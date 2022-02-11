fd7e172e35509b8c9794d777d383e20a6fbf13a9
Julio <juliojm1310@gmail.com>
2022-02-11 11:55:22 +0300

    homework from lesson 2

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,13 +1,34 @@
 from django.shortcuts import render
+from datetime import datetime
+import json
 
 # Create your views here.
+with open('context.json', 'r', encoding='utf-8') as f:
+   contex_json = json.load(f)
+
+mainapp_list = contex_json['mainapp_list']
+
 def index(request):
-    return render(request, 'mainapp/index.html')
+    return render(request, 'mainapp/index.html', context={
+        'mainapp_list': mainapp_list,
+        'now_date': datetime.now(),
+        'name': 'jarno',
+    })
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    products_category = contex_json['products_category']
+    return render(request, 'mainapp/products.html', context={
+        'products_category': products_category,
+        'mainapp_list': mainapp_list,
+        'now_date': datetime.now(),
+        'name': 'jarno',
+    })
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    return render(request, 'mainapp/contact.html', context={
+        'mainapp_list': mainapp_list,
+        'now_date': datetime.now(),
+        'name': 'jarno',
+    })
