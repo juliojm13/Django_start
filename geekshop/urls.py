eb1d27ab709119a6e3ad2bc7bf5dd1604e83f399
Julio <juliojm1310@gmail.com>
2022-02-13 13:20:15 +0300

    homework from lesson 3

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -16,9 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from mainapp import views as mainapp_views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import include
+
+
 urlpatterns = [
     path('', mainapp_views.index, name ='index'),
-    path('products/', mainapp_views.products, name = 'products'),
+    path('products/', include('mainapp.urls' ,namespace = 'products')),
     path('contact/', mainapp_views.contact, name = 'contact'),
     path('admin/', admin.site.urls),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=60,unique=True,verbose_name='Category name')
+    description = models.TextField(blank=True,verbose_name='Category descrition')
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    name = models.CharField(max_length=70,verbose_name='Product name')
+    image = models.ImageField(upload_to= 'products_images', blank=True)
+    short_description = models.TextField(max_length=60,blank=True,verbose_name='products short description')
+    description = models.TextField(max_length=250, blank=True,verbose_name='products description')
+    price = models.DecimalField(max_digits=8,decimal_places=2,default=0,verbose_name='products price')
+    quantity = models.PositiveIntegerField(verbose_name='quantity of products available',default=0)
+
+
+    def __str__(self):
+        return self.name
